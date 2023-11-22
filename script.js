83c3f681698fd5b74cac83b4892b5fdbd2942b96
DL23727272 <113783762+DL23727272@users.noreply.github.com>
2023-11-23 01:26:51 +0800

    no words

---
+++ script.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const lyrics = [
+        "Tatlong bilyon",
+        "ikaw lang nga ang aking gusto",
+        "Pasensiya na kung ngayon",
+        "ako'y 'di para sa 'yo",
+        "Tayo ay papunta na",
+        "sa 'ting bagong yugto",
+        "Yokong mabuhay sa isang mundong walang tayo"
+    ];
+
+    const delay = 75; // adjust the speed here
+    const lyricsElement = document.getElementById("lyrics");
+
+    async function displayLyrics() {
+        for (const line of lyrics) {
+            for (const char of line) {
+                const charElement = document.createElement("span"); 
+                charElement.textContent = char;
+                charElement.style.animation = "glow 2s ease-in-out"; 
+                lyricsElement.appendChild(charElement); 
+
+                await new Promise((resolve) => setTimeout(resolve, delay));
+
+                
+                charElement.style.animation = "";
+            }
+
+            lyricsElement.appendChild(document.createElement("br")); 
+
+            await new Promise((resolve) => setTimeout(resolve, delay * 10));
+
+            lyricsElement.textContent = "";
+
+            await new Promise((resolve) => setTimeout(resolve, delay * 10));
+        }
+
+        setTimeout(function () {
+            window.location.href = "card.html"; //set here your next target html
+        }, 32000);
+    }
+    
+    displayLyrics();
+});
